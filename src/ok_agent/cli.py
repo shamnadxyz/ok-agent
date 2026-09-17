ec3b0704f30a8ce0bc9a6e3d6f13271eb229cfb2
shamnadxyz <shamnadxyz@proton.me>
2026-09-17 07:17:09 +0530

    Add readline and input history

--- src/ok_agent/cli.py
+++ src/ok_agent/cli.py
@@ -7,14 +7,15 @@ from ok_agent.tools.read_tool import read_tool
 from ok_agent.tools.shell_tool import shell_tool
 from ok_agent.tools.utils import get_tools_schema, handle_tool_calls
 from ok_agent.tools.write_tool import write_tool
-from ok_agent.utils import get_system_prompt
+from ok_agent.utils import get_system_prompt, setup_history
 
 
 def cli():
     messages: list[Message] = [get_system_prompt()]
 
-    selected_tools = [shell_tool, read_tool, write_tool, edit_tool]
-    tool_schemas, tools = get_tools_schema(selected_tools)
+    tool_schemas, tools = get_tools_schema(
+        [shell_tool, read_tool, write_tool, edit_tool]
+    )
 
     print(f"{BOLD}Ok Agent{RESET}")
 
@@ -52,6 +53,7 @@ def cli():
 
 def main():
     setup_logging()
+    setup_history()
     try:
         cli()
     except (EOFError, KeyboardInterrupt, SystemExit):
--- src/ok_agent/utils.py
+++ src/ok_agent/utils.py
@@ -1,9 +1,30 @@
+import atexit
+import readline
 from logging import getLogger
 from pathlib import Path
 
 from ok_agent.openai.types import ContentPartText, SystemMessage
 
 logger = getLogger(__name__)
+
+
+def setup_history():
+    histfile = Path.home() / ".ok_history"
+    history_length = 1000
+
+    try:
+        readline.read_history_file(histfile)
+        h_len = readline.get_current_history_length()
+    except FileNotFoundError:
+        histfile.touch()
+        h_len = 0
+
+    def save(prev_h_len, histfile):
+        new_h_len = readline.get_current_history_length()
+        readline.set_history_length(history_length)
+        readline.append_history_file(new_h_len - prev_h_len, histfile)
+
+    atexit.register(save, h_len, histfile)
 
 
 def get_system_prompt() -> SystemMessage:
